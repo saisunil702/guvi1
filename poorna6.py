3561ceccfc98eb4f0e4485d792bc911227e888c6
pedireddy <41773732+pedireddy@users.noreply.github.com>
2018-07-27 15:46:05 +0530

    Update poorna6.py

--- poorna6.py
+++ poorna6.py
@@ -1,6 +1,6 @@
-num1=float(raw_input())
-num2=float(raw_input())
-num3=float(raw_input())
+num1=int(raw_input())
+num2=int(raw_input())
+num3=int(raw_input())
 if(num1>num2 and num1>num3):
 	print(num1)
 elif(num2>num1 and num2>num3):
