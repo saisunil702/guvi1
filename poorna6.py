ba01ee9204a6364441a86eed77324263c6533aa9
pedireddy <41773732+pedireddy@users.noreply.github.com>
2018-07-27 15:54:38 +0530

    Update poorna6.py

--- poorna6.py
+++ poorna6.py
@@ -1,6 +1,4 @@
-num1=int(raw_input())
-num2=int(raw_input())
-num3=int(raw_input())
+num1, num2, num3=map(int, raw_input().split())
 if(num1>num2 and num1>num3):
 	print(num1)
 elif(num2>num1 and num2>num3):
